6d83551236ae10bad85db3c5d8841fb31518e783
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2020-03-04 04:29:59 +0100

    feature(homepage):implement homepage - implement `Home`and `Banner` components [Finishes #2]

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,7 @@ import classes from './Banner.module.scss';
 
 export default ({ height }) => {
   return (
-    <div className={classes.Banner} style={{ height: height || '500px' }}>
+    <div className={classes.Banner} style={{ height: height || '400px' }}>
       <div className={`image-wrapper`}>
         <img src={bannerImg} alt="" />
       </div>
